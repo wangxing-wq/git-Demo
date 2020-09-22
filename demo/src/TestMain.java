ff39754f5d4732325e4e3ca9f6fa58803257a5fc
wangxing-wq <2234346829@qq.com>
2020-09-22 14:26:58 +0800

    主分支修改1 增加了hello master

--- demo/src/TestMain.java
+++ demo/src/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello master");
+        System.out.println("hello masterg");
     }
 
 }
